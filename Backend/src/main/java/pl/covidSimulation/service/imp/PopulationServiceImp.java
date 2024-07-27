76ab2fafc165eddad66e3c8ff489b65a746f80c2
PolekM <marek.pawel.polek@gmail.com>
2024-07-27 11:35:36 +0200

    remove unecessery verible

--- Backend/src/main/java/pl/covidSimulation/service/imp/PopulationServiceImp.java
+++ Backend/src/main/java/pl/covidSimulation/service/imp/PopulationServiceImp.java
@@ -23,75 +23,70 @@ public class PopulationServiceImp implements PopulationService {
     }
 
     @Transactional
-    public void createSimulationData(SimulationData simulationData){
-
-        int days = simulationData.getTs();
-        int daysToDead = simulationData.getTm();
-        int daysToHeal = simulationData.getTi();
+    public void createSimulationData(SimulationData simulationData) {
 
         List<Population> covidSimulationDataList = new ArrayList<>();
 
-        Integer[] newInfection = new Integer[days];
-        Integer[] newRecoveries = new Integer[days];
-        Integer[] newDeaths = new Integer[days];
-        fillArrays(newInfection,newRecoveries,newDeaths);
+        Integer[] newInfection = new Integer[simulationData.getTs()];
+        Integer[] newRecoveries = new Integer[simulationData.getTs()];
+        Integer[] newDeaths = new Integer[simulationData.getTs()];
+        fillArrays(newInfection, newRecoveries, newDeaths);
 
-        initFirstPopulation(simulationData,covidSimulationDataList,newInfection);
-        createAllDay(days,daysToDead,daysToHeal,newInfection,newDeaths,newRecoveries,simulationData,covidSimulationDataList);
+        initFirstPopulation(simulationData, covidSimulationDataList, newInfection);
+        createAllDay(newInfection, newDeaths, newRecoveries, simulationData, covidSimulationDataList);
         populationRepository.saveAll(covidSimulationDataList);
 
     }
 
-    public void fillArrays(Integer[] newInfection, Integer[] newRecoveries, Integer[] newDeaths){
-        Arrays.fill(newInfection,0);
-        Arrays.fill(newRecoveries,0);
-        Arrays.fill(newDeaths,0);
+    public void fillArrays(Integer[] newInfection, Integer[] newRecoveries, Integer[] newDeaths) {
+        Arrays.fill(newInfection, 0);
+        Arrays.fill(newRecoveries, 0);
+        Arrays.fill(newDeaths, 0);
     }
 
-    public void initFirstPopulation(SimulationData simulationData, List<Population> covidSimulationDataList,Integer[] newInfection){
+    public void initFirstPopulation(SimulationData simulationData, List<Population> covidSimulationDataList, Integer[] newInfection) {
         Population firstPopulation = new Population(simulationData);
         covidSimulationDataList.add(firstPopulation);
         newInfection[0] = firstPopulation.getPi();
     }
 
-    public void createAllDay(Integer days, Integer daysToDead, Integer daysToHeal, Integer[] newInfection, Integer[] newDeaths, Integer[] newRecoveries, SimulationData simulationData, List<Population> covidSimulationDataList){
-        for(int i=1;i<days;i++){
-            Population currentDate = new Population(covidSimulationDataList.get(i-1));
-            if(daysToDead<=i){
-                newDeadPeople(daysToDead,i,newInfection,newDeaths,simulationData,currentDate);
+    public void createAllDay(Integer[] newInfection, Integer[] newDeaths, Integer[] newRecoveries, SimulationData simulationData, List<Population> covidSimulationDataList) {
+        for (int i = 1; i < simulationData.getTs(); i++) {
+            Population currentDate = new Population(covidSimulationDataList.get(i - 1));
+            if (simulationData.getTm() <= i) {
+                newDeadPeople(i, newInfection, newDeaths, simulationData, currentDate);
             }
-            if(daysToHeal<=i) {
-                newRecoveredPeople(newInfection,i,daysToHeal,simulationData,newRecoveries,currentDate);
+            if (simulationData.getTi() <= i) {
+                newRecoveredPeople(newInfection, i, simulationData, newRecoveries, currentDate);
             }
-            newInfectedPeople(currentDate,simulationData,newInfection,i);
-
+            newInfectedPeople(currentDate, simulationData, newInfection, i);
             covidSimulationDataList.add(currentDate);
 
         }
     }
 
-    public void newDeadPeople(Integer daysToDead, Integer i, Integer[] newInfection, Integer[] newDeaths, SimulationData simulationData, Population currentDate){
+    public void newDeadPeople(Integer i, Integer[] newInfection, Integer[] newDeaths, SimulationData simulationData, Population currentDate) {
 
-            int infectedThatDay = newInfection[i-daysToDead];
-            int currentDead = (int) (infectedThatDay * simulationData.getM());
-            newDeaths[i] = currentDead;
-            currentDate.setPm(currentDate.getPm() + currentDead);
-            currentDate.setPi(currentDate.getPi() - currentDead);
+        int infectedThatDay = newInfection[i - simulationData.getTm()];
+        int currentDead = (int) (infectedThatDay * simulationData.getM());
+        newDeaths[i] = currentDead;
+        currentDate.setPm(currentDate.getPm() + currentDead);
+        currentDate.setPi(currentDate.getPi() - currentDead);
 
-        }
-
-    public void newRecoveredPeople(Integer[] newInfection, Integer i, Integer daysToHeal, SimulationData simulationData, Integer[] newRecoveries, Population currentDate){
-        int infectedThatDay = newInfection[i-daysToHeal];
-        int currentHeal = (int) (infectedThatDay - infectedThatDay * simulationData.getM());
-        newRecoveries[i] = currentHeal;
-        currentDate.setPr(currentHeal+currentDate.getPr());
-        currentDate.setPi(currentDate.getPi()-currentHeal);
     }
 
-    public void newInfectedPeople(Population currentDate, SimulationData simulationData, Integer[] newInfection, Integer i){
-        int currentInfected = Math.min(currentDate.getPv(),currentDate.getPi()*simulationData.getR());
-        currentDate.setPi(currentInfected+currentDate.getPi());
-        currentDate.setPv(currentDate.getPv()-currentInfected);
+    public void newRecoveredPeople(Integer[] newInfection, Integer i, SimulationData simulationData, Integer[] newRecoveries, Population currentDate) {
+        int infectedThatDay = newInfection[i - simulationData.getTi()];
+        int currentHeal = (int) (infectedThatDay - infectedThatDay * simulationData.getM());
+        newRecoveries[i] = currentHeal;
+        currentDate.setPr(currentHeal + currentDate.getPr());
+        currentDate.setPi(currentDate.getPi() - currentHeal);
+    }
+
+    public void newInfectedPeople(Population currentDate, SimulationData simulationData, Integer[] newInfection, Integer i) {
+        int currentInfected = Math.min(currentDate.getPv(), currentDate.getPi() * simulationData.getR());
+        currentDate.setPi(currentInfected + currentDate.getPi());
+        currentDate.setPv(currentDate.getPv() - currentInfected);
         newInfection[i] = currentInfected;
     }
 
